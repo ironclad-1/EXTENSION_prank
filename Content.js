c94f1dd1da74afc7c9e3f8e9886092e17a31668a
ASHRAAF <130046079+ironclad-1@users.noreply.github.com>
2024-03-19 21:06:39 +0530

    Add files via upload

---
+++ Content.js
@@ -0,0 +1,32 @@
+let bobRossImages = [
+  "https://pbs.twimg.com/media/Ewhn4wgU8AwH3Nd.jpg"
+];
+
+// Function to check if an element is in the viewport
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to lazy load images
+function lazyLoadImages() {
+  const imgs = document.getElementsByTagName("img");
+
+  for (image of imgs) {
+      if (isElementInViewport(image)) {
+          const index = Math.floor(Math.random() * bobRossImages.length);
+          image.src = bobRossImages[index];
+      }
+  }
+}
+
+// Add event listener for scroll to lazy load images
+window.addEventListener("scroll", lazyLoadImages);
+
+// Lazy load images on initial page load
+lazyLoadImages();
